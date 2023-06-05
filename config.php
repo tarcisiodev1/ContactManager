4e38b0d965245a449d16d8a2f8459a2fd13dd24a
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-06-05 00:06:51 -0300

    configs

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 define('MYSQL_CONFIG', [
     'host' => 'localhost',
     'database' => 'php_pdo_contatos',
-    'username' => '',
-    'password' => '',
+    'username' => 'root',
+    'password' => '4et53rterwefwtr434gret53452hrthrt3fdfgj3453ergrr42',
 ]);
