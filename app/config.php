d922c0cd7fce18339646f4b1d753ca30a6ba1b85
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-06-11 00:29:04 -0300

    Remover

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@ define('APP_NAME',  'App Name');
 // database
 define('MYSQL_HOST',        'localhost');
 define('MYSQL_DATABASE',    'php_pdo_contatos');
-define('MYSQL_USERNAME',    '');
+define('MYSQL_USERNAME',    'root');
 define('MYSQL_PASSWORD',    '');
 
 define('MYSQL_AES_KEY',     '');
